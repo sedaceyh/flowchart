5137b00da5e9282a5001d07c299baec06d5f3f0d
sedaceyh <sedaceyh5769@gmail.com>
2024-10-06 16:04:32 +0300

    Add files via upload

---
+++ geo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int c = 0;
+        double p = 1.0;
+        Console.WriteLine("Sayı Adeti Girin: ");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        while (c < count)
+        {
+            Console.WriteLine("Gerçek Sayı Girin: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            c = c + 1;
+            p = p * x;
+        }
+
+        double gm = Math.Pow(p, 1.0 / count);
+        Console.WriteLine("Ortalama: " + gm);
+    }
+}
+
