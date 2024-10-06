0c4488e7add36ff23d151cfd44b6b56e97da744a
sedaceyh <sedaceyh5769@gmail.com>
2024-10-06 16:03:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿{
+    Console.WriteLine("ad");
+}
+Console.ReadLine();
+Console.WriteLine("soyad");
+Console.ReadLine();
+Console.WriteLine("ogrenci numaraai");
+Console.ReadLine();
+Console.WriteLine("cep telefon numarasi");
+Console.ReadLine();
+Console.WriteLine("e-mail adresi");
+Console.ReadLine();
+
+
+
+
